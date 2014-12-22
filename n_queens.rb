c0f9c2ef3a8ff39d6717a698241e644f9aba1455
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2014-12-22 18:39:00 +0100

    Cleanup

--- n_queens.rb
+++ n_queens.rb
@@ -4,13 +4,15 @@
 # USAGE: ruby n_queens.rb <NQUEENS>
 
 def check_along_dimension candidate
-  candidate.transpose.map do |lst| 
+  candidate.transpose.map do |lst|
     lst.uniq.length == lst.length
   end.all?
 end
 
 def rotate_coordinates candidate_coordinates
-  candidate_coordinates.map { |c| [c.reduce(:-), c.reduce(:+)]  }
+  candidate_coordinates.map do |c|
+    [c.reduce(:-), c.reduce(:+)]
+  end
 end
 
 def to_coordinates candidate
@@ -18,33 +20,37 @@ def to_coordinates candidate
 end
 
 def check_solution candidate
-  check_along_dimension rotate_coordinates to_coordinates candidate
+  check_along_dimension  rotate_coordinates  to_coordinates  candidate
 end
 
 def solve boardsize
   (1..boardsize).to_a.permutation.select do |candidate|
-      check_solution(candidate) && print_solution(candidate)
+    check_solution(candidate).tap do |ans|
+      print_solution(candidate) if ans
     end
+  end
 end
 
-# Some pretty printing
+# That's all. Now for some pretty printing
 
-def baseline(boardsize); ". " * boardsize; end
-class String; def sub_at(n,i); self[n] = i; self; end; end
+module SubAt; def sub_at(n,i); self.tap { |s| s[n] = i }; end; end
+def baseline(boardsize); (". " * boardsize).extend SubAt; end
 def board_line(qpos, boardsize); baseline(boardsize).sub_at((qpos-1)*2, "x"); end
 
 def print_solution solution
-  coordinates = to_coordinates solution
+  coordinates = to_coordinates  solution
   puts "\n-> " + coordinates.to_s
   cols = (coordinates.sort { |a, b| a.first <=> b.first }).transpose.last
-  cols.each { |c| puts board_line c, coordinates.length }
-  true # trick to simplify `#solve`
+  cols.each { |c| puts board_line(c, coordinates.length) }
 end
 
-# MAIN execution
+# Main execution
 
-raise ArgumentError, "USAGE: ruby n_queens.rb <NQUEENS>" unless ARGV.size==1
+if __FILE__==$0
+  raise ArgumentError, "USAGE: ruby n_queens.rb <NQUEENS>" unless ARGV.size==1
+  nqueens = ARGV.first.to_i
 
-puts "\nSolutions found:"
-sols = solve(ARGV.first.to_i)
-puts "\nTotal: #{sols.size} solutions found."
+  puts "\nSolving n-queens problem of size #{nqueens}:"
+  sols = solve(nqueens)
+  puts "\nTotal: #{sols.size} solutions found."
+end
